46b2f6dfeec4508d0282fad5b9b0024a0d5afdbf
gmemstr <bladesimmer@gmail.com>
2017-12-25 19:47:10 -0800

    Added build batch script (Windows)

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+REM This script will build then aggregating the dist files for the two directories into one for release - WINDOWS
+IF EXIST dist (
+	echo Final dist folder found
+) ELSE (
+	echo Creating dist folder
+	mkdir dist
+)
+echo Building admin
+cd admin
+call npm run build
+echo Copying admin files
+xcopy dist ..\dist /s /e /y
+cd ..
+cd main
+echo Building main
+call npm run build
+echo Copying main files
+xcopy dist ..\dist /s /e /y
+cd ..
+echo Done
